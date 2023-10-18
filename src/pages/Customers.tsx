0f63cee0592c0f1e15fe6a57e8becd9bdc125e6f
GyaneshwerJha <98211593+GyaneshwerJha@users.noreply.github.com>
2023-10-18 12:53:10 +0530

    day-1

---
+++ src/pages/Customers.tsx
@@ -0,0 +1,13 @@
+import AdminSidebar from "../components/AdminSidebar";
+
+const Customers = () => {
+  return (
+    <div className="admin-container">
+      <AdminSidebar />
+      <main></main>
+      {/* Main */}
+    </div>
+  );
+};
+
+export default Customers;
